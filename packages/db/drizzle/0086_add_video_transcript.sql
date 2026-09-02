92753e5662c2804ca50ecc7860df32210bb25866
Tokarzewski <bartlomiej.tokarzewski@gmail.com>
2026-09-02 16:57:48 +0200

    fix: remove renamed migration files that were replaced by 0094/0095

--- packages/db/drizzle/0086_add_video_transcript.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `bookmarkLinks` ADD `transcript` text;--> statement-breakpoint
-ALTER TABLE `bookmarkLinks` ADD `transcriptionStatus` text;
--- packages/db/drizzle/0087_add_digest_enabled.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `user` ADD `digestEnabled` integer DEFAULT false NOT NULL;
